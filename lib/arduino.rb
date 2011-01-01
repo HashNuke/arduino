a28494db11e6526bb5f9e022e87406f55a16ce86
Akash Manohar J <akash@akash.im>
2011-01-01 23:39:48 +0530

    v0.3 source
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/arduino.rb
+++ lib/arduino.rb
@@ -1,18 +1,35 @@
+# The program takes an initial word or phrase from
+# the command line (or in the absence of a
+# parameter from the first line of standard
+# input).  In then reads successive words or
+# phrases from standard input and reports whether
+# they are angrams of the first word.
+#
+# Author::    Dave Thomas  (mailto:dave@x.y)
+# Copyright:: Copyright (c) 2002 The Pragmatic Programmers, LLC
+# License::   Distributes under the same terms as Ruby
+
 require "serialport"
+
+# The main Arduino class.
+# Allows managing connection to board and getting & setting pin states.
 
 class Arduino
 
+	# initialize port and baudrate
 	def initialize(port, baudrate=115200)
 	  @serial = SerialPort.new port, baudrate
 	  @serial.sync
-	  @port = port	#Cannot get connected port via SerialPort class
+	  @port = port
 	  @outputPins = []
 	end
 
+	# Print information about connected board
 	def to_s
 	  "Arduino is on port #{@port} at #{@serial.baud} baudrate"
 	end
-
+	
+	# Set output pins. This is a must.
 	def output(*pinList)
 	  sendData(pinList.length)
 	
@@ -26,22 +43,26 @@ class Arduino
 	  end
 	end
 
+	# Get state of a digital pin. Returns true if high and false if low.
 	def getState(pin)
 	  sendData('2')
 	  sendPin(pin)
 	  return formatPinState(getData())
 	end
 
+	# Set a pin state to low
 	def setLow(pin)
 	  sendData('0')
 	  sendPin(pin)
 	end
-
+	
+	# Set a pin state to high
 	def setHigh(pin)
 	  sendData('1')
 	  sendPin(pin)
 	end
-
+	
+	# Write to an analog pin
 	def analogWrite(pin, value)
 		sendData('3')
 	  fullHexValue = value.to_s(base=16)
@@ -54,18 +75,21 @@ class Arduino
 	  sendData(hexValue[1])
 	end
 
+	# Read from an analog pin
 	def analogRead(pin)
 	  sendData('4')
 	  sendPin(pin)
 	  getData()
 	end
 
+	# set all pins to low
 	def turnOff
 	  @outputPins.each do |pin|
 	    setLow(pin)
 	  end
 	end
 
+	# close serial connection to connected board
 	def close
 		@serial.close
 	end
@@ -93,7 +117,6 @@ class Arduino
 	def formatPinState(pinValue)
 	  return true if pinValue=="1"
 	  return false #if pinValue=="0"
-	  #raise Exception, "Data/Connection error"
 	end
 
 end
